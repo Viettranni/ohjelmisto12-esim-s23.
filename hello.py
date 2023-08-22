44402393dd6fcd942402bdaf927b2c094c20088f
Viettranni <127795338+Viettranni@users.noreply.github.com>
2023-08-22 10:25:46 +0300

    Moduuli 01

--- hello.py
+++ hello.py
@@ -2,3 +2,17 @@ nimi = input("Anna nimi!")
 
 print(f"Morjesta,\n{nimi}!")
 print("Mitä kuuluu?")
+
+age = 6 + 6
+print(age)
+
+age = int(input("Mikä on oikea ikäsi?"))
+
+if age >= 18:
+    print("Olet täysi-ikäinen!")
+else:
+    print("Mene kouluun")
+
+numeroita = 6, 7, 8, 9
+
+print(numeroita[0])
