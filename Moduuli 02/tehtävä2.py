9228b0ec47a0a4b48c5ae6bf28652c654652e786
Viettranni <127795338+Viettranni@users.noreply.github.com>
2023-08-22 10:58:16 +0300

    Moduuli 02

--- Moduuli 02/tehtävä2.py
+++ Moduuli 02/tehtävä2.py
@@ -3,4 +3,4 @@ import math
 sade = float(input("Mikä on ympyrän säde? "))
 pinta = math.pi * sade**2
 
-print(f"Ympyrän pinta-ala on: {pinta:.2f} neliömetriä.")
+print(f"Ympyrän pinta-ala on: {pinta} neliömetriä.")
