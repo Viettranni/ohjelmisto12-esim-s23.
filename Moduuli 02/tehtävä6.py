6790fca049cdb3d0019f707adbeba2b7fa645adc
Viettranni <127795338+Viettranni@users.noreply.github.com>
2023-08-22 14:01:02 +0300

    Moduuli 02

--- Moduuli 02/tehtävä6.py
+++ Moduuli 02/tehtävä6.py
@@ -2,14 +2,14 @@ import random
 
 def kolmenkoodi():
     code = ""
-    for _ in range(3):
+    for i in range(3):
         numeroa = str(random.randint(0, 9))
         code += numeroa
     return code
 
 def neljankoodi():
     code = ""
-    for _ in range(4):
+    for i in range(4):
         numeroa = str(random.randint(1, 6))
         code += numeroa
     return code
